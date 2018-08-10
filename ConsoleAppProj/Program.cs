8229eccc9f4a57fe082492ddd674f8d52b6aea07
Ravi K Sinha <ravisinha@in.unibiz.com>
2018-08-10 16:29:57 +0530

    Initial commit with Skeleton Console project

---
+++ ConsoleAppProj/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
